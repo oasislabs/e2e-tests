3937e5af4294ebeaf4ec6f737beadec269081523
Nick Hynes <nhynes@oasislabs.com>
2019-02-21 21:58:13 +0000

    Export wasm_ctors

--- contracts/tvm-basic/build.rs
+++ contracts/tvm-basic/build.rs
@@ -12,15 +12,30 @@ fn main() {
     .expect("Failed to build TVM lib");
 
     let obj_path = std::path::PathBuf::from(format!("{}/test.o", out_dir));
+    let obj_path_str = obj_path.to_str().unwrap();
     assert!(
         obj_path.exists(),
         "Could not build tvm lib: {}",
         String::from_utf8(output.stderr).unwrap()
     );
 
+    let output = Command::new("ld.lld-7")
+        .args(&[
+            "-flavor",
+            "wasm",
+            "-o",
+            &format!("{}.o.o", obj_path_str),
+            "--no-entry",
+            "--allow-undefined",
+            "--export=__wasm_call_ctors",
+            obj_path_str,
+        ])
+        .output()
+        .expect("Failed to export symbols");
+
     let lib_path = std::path::PathBuf::from(format!("{}/libtest.a", out_dir));
     let output = Command::new("llvm-ar-7")
-        .args(&["r", lib_path.to_str().unwrap(), obj_path.to_str().unwrap()])
+        .args(&["r", lib_path.to_str().unwrap(), obj_path_str])
         .output()
         .expect("Failed to create library archive");
     assert!(
