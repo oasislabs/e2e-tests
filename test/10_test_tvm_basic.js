e2a5662e5df56cb2b39a8db264b0ba0d54aeaa95
Nick Hynes <nhynes@oasislabs.com>
2019-02-23 01:03:56 +0000

    Fix js style

--- test/10_test_tvm_basic.js
+++ test/10_test_tvm_basic.js
@@ -11,7 +11,7 @@ if (truffleConfig.shouldRun(__filename)) {
       const contract = new web3.eth.Contract(BasicTVM.abi);
       let instance = await contract.deploy({ data: BasicTVM.bytecode }).send({
         from: accounts[0],
-        gas: '0xf00000', // XXX: works around estimateGas producing 2^51
+        gas: '0xf00000' // XXX: works around estimateGas producing 2^51
       });
       const bytes = await instance.methods.test().call();
       assert.equal(bytes, '1');
