1d8d687e026bfdce3eb3566109114a2a24326388
armaniferrante <armaniferrante@gmail.com>
2019-03-02 00:32:31 +0000

    Test updates

--- test/11_test_confidential_cross_contract.js
+++ test/11_test_confidential_cross_contract.js
@@ -10,117 +10,156 @@ if (truffleConfig.shouldRun(__filename)) {
   contract('Confidential Cross Contract Calls', function (accounts) {
     const options = { from: accounts[0] };
 
-	let callableContract  = new web3c.oasis.Contract(CallableCounter.abi, undefined, options);
+    let callableContract = new web3c.oasis.Contract(CallableCounter.abi, undefined, options);
 
-	let a = undefined;
-	let b = undefined;
+    let a;
+    let b;
 
-	it ('sets up non-confidential contracts', async () => {
-	  b = await callableContract.deploy({
-		data: CallableCounter.bytecode,
-		arguments: [accounts[0], 1], // This arg doesnt matter.
-		header: {
-		  confidential: false
-		}
-	  }).send();
-	  a = await callableContract.deploy({
-		data: CallableCounter.bytecode,
-		arguments: [b.options.address, 0],
-		header: {
-		  confidential: false
-		}
-	  }).send();
-	});
-
-	it('non-confidential calls non-confidential', async () => {
-	  // Given contracts A and B.
-
-	  // When.
-	  let aPeerCount = await a.methods.getPeerCounter().call();
-
-	  // Then.
-	  assert.equal(aPeerCount, 1);
-	});
-
-	it('non-confidential sets non-confidential storage via call', async () => {
-	  // Given, contracts A and B.
-
-	  // When.
-	  await a.methods.incrementPeerCounter().send();
-
-	  // Then.
-	  let aCount = await a.methods.getCounter().call();
-	  let bCount = await b.methods.getCounter().call();
-
-	  let aSender = await a.methods._sender().call();
-	  let bSender = await b.methods._sender().call();
-
-	  // Unchanged.
-	  assert.equal(aCount, 0);
-	  // Incremented.
-	  assert.equal(bCount, 2);
-	  // Sender is originator of transaction.
-	  assert.equal(aSender, accounts[0]);
-	  // Sender is contract A because it invoked a call.
-	  assert.equal(bSender, a.options.address);
-	});
-
-	it('non-confidential sets non-confidential storage via delegatecall', async () => {
-	  // Given, contracts A and B.
-
-	  // When.
-	  let r = await a.methods.delegatecallIncrementCounter().send();
-
-	  // Then.
-	  let aCount = await a.methods.getCounter().call();
-	  let bCount = await b.methods.getCounter().call();
-
-	  let aSender = await a.methods._sender().call();
-	  let bSender = await b.methods._sender().call();
-
-	  // Incremented.
-	  assert.equal(aCount, 1);
-	  // Unchanged.
-	  assert.equal(bCount, 2);
-	  // Sender is originator of transaction.
-	  assert.equal(aSender, accounts[0]);
-	  // Sender is contract A because it invoked a call.
-	  assert.equal(bSender, a.options.address);
-	});
-
-	it('non-confidential sets non-confidential storage via callcode', async () => {
-	  // todo
-	});
-
-	it ('sets up confidential contracts', async () => {
-	  b = await callableContract.deploy({
-		data: CallableCounter.bytecode,
-		arguments: [accounts[0], 1], // This arg doesnt matter.
-		header: {
-		  confidential: true
-		}
-	  }).send();
-	  a = await callableContract.deploy({
-		data: CallableCounter.bytecode,
-		arguments: [b.options.address, 0],
-		header: {
-		  confidential: true
-		}
-	  }).send();
-	});
-
-
-	it('confidential calls confidential', async () => {
-	  // Given contracts A and B.
-
-	  // When.
-	  let aPeerCount = await a.methods.getPeerCounter().call();
-
-	  // Then.
-	  assert.equal(aPeerCount, 1);
+    it('sets up non-confidential contracts', async () => {
+      b = await callableContract.deploy({
+        data: CallableCounter.bytecode,
+        arguments: [accounts[0], 1], // This arg doesnt matter.
+        header: {
+          confidential: false
+        }
+      }).send();
+      a = await callableContract.deploy({
+        data: CallableCounter.bytecode,
+        arguments: [b.options.address, 0],
+        header: {
+          confidential: false
+        }
+      }).send();
     });
 
-	it('confidential calls confidential and sets storage', async () => {
+    it('non-confidential calls non-confidential', async () => {
+      // Given contracts A and B.
+
+      // When.
+      let aPeerCount = await a.methods.getPeerCounter().call();
+
+      // Then.
+      assert.equal(aPeerCount, 1);
+    });
+
+    it('non-confidential sets non-confidential storage via call', async () => {
+      // Given, contracts A and B.
+
+      // When.
+      await a.methods.incrementPeerCounter().send();
+
+      // Then.
+      let aCount = await a.methods.getCounter().call();
+      let bCount = await b.methods.getCounter().call();
+
+      let aSender = await a.methods._sender().call();
+      let bSender = await b.methods._sender().call();
+
+      // Unchanged.
+      assert.equal(aCount, 0);
+      // Incremented.
+      assert.equal(bCount, 2);
+      // Sender is originator of transaction.
+      assert.equal(aSender, accounts[0]);
+      // Sender is contract A because it invoked a call.
+      assert.equal(bSender, a.options.address);
+    });
+
+    it('non-confidential sets non-confidential storage via delegatecall', async () => {
+      // Given, contracts A and B.
+
+      // When.
+      await a.methods.delegatecallIncrementCounter().send();
+
+      // Then.
+      let aCount = await a.methods.getCounter().call();
+      let bCount = await b.methods.getCounter().call();
+
+      let aSender = await a.methods._sender().call();
+      let bSender = await b.methods._sender().call();
+
+      // Incremented.
+      assert.equal(aCount, 1);
+      // Unchanged.
+      assert.equal(bCount, 2);
+      // Sender is originator of transaction.
+      assert.equal(aSender, accounts[0]);
+      // Sender is contract A because it invoked a call.
+      assert.equal(bSender, a.options.address);
+    });
+
+    it('non-confidential sets non-confidential storage via callcode', async () => {
+      // todo
+    });
+
+    it('sets up confidential contracts', async () => {
+      b = await callableContract.deploy({
+        data: CallableCounter.bytecode,
+        arguments: [accounts[0], 1], // This arg doesnt matter.
+        header: {
+          confidential: true
+        }
+      }).send();
+      a = await callableContract.deploy({
+        data: CallableCounter.bytecode,
+        arguments: [b.options.address, 0],
+        header: {
+          confidential: true
+        }
+      }).send();
+    });
+
+    it('confidential calls confidential', async () => {
+      // Given contracts A and B.
+
+      // When.
+      let aCount = await a.methods.getCounter().call();
+      let aPeerCount = await a.methods.getPeerCounter().call();
+
+      // Then.
+      assert.equal(aCount, 0);
+      assert.equal(aPeerCount, 1);
+    });
+
+    it('confidential calls confidential and sets storage', async () => {
+      // Given contracts A and B.
+
+      // When.
+      await a.methods.incrementPeerCounter().send();
+
+      // Then.
+      let aCount = await a.methods.getCounter().call();
+      let aPeerCount = await a.methods.getPeerCounter().call();
+
+      // Then.
+      assert.equal(aCount, 0);
+      assert.equal(aPeerCount, 2);
+    });
+
+    it('confidential sets confidential storage via delegatecall', async () => {
+      // Given, contracts A and B.
+
+      // When.
+      await a.methods.delegatecallIncrementCounter().send();
+
+      // Then.
+      let aCount = await a.methods.getCounter().call();
+      let bCount = await b.methods.getCounter().call();
+
+      let aSender = await a.methods._sender().call();
+      let bSender = await b.methods._sender().call();
+
+      // Incremented.
+      assert.equal(aCount, 1);
+      // Unchanged.
+      assert.equal(bCount, 2);
+      // Sender is originator of transaction.
+      assert.equal(aSender, accounts[0]);
+      // Sender is contract A because it invoked a call.
+      assert.equal(bSender, a.options.address);
+    });
+
+    it('confidential creates non-confidential', async () => {
 
     });
 
@@ -128,15 +167,15 @@ if (truffleConfig.shouldRun(__filename)) {
 
     });
 
-	it('confidential calls non-confidential and sets storage', async () => {
+    it('confidential calls non-confidential and sets storage', async () => {
 
     });
 
-	it('non-confidential confidential', async () => {
+    it('non-confidential confidential', async () => {
 
     });
 
-	it('non-confidential confidential and sets storaage', async () => {
+    it('non-confidential confidential and sets storaage', async () => {
 
     });
   });
