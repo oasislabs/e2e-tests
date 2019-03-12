ea72c49fb9d83a30ba108eeb8367321b91b07869
armaniferrante <armaniferrante@gmail.com>
2019-03-12 02:07:15 +0000

    More testing

--- contracts/cross_contract/confidential/CallableCounter.sol
+++ contracts/cross_contract/confidential/CallableCounter.sol
@@ -1,5 +1,6 @@
 pragma solidity 0.4.25;
 
+/// CallableCounter is a linked list of counter contracts that can call each other.
 contract CallableCounter {
 
   /// The peer that I can call.
@@ -8,12 +9,15 @@ contract CallableCounter {
   address public _sender;
   /// The current count.
   uint256 public _count;
+  /// True iff this is the last counter in a linked ist.
+  bool public _isTail;
 
   event Status(bool status);
 
-  constructor(address peer, uint256 count) public {
+  constructor(address peer, uint256 count, bool isTail) public {
 	_peer = CallableCounter(peer);
 	_count = count;
+	_isTail = isTail;
   }
 
   function incrementCounter() public {
@@ -21,6 +25,7 @@ contract CallableCounter {
 	_sender = msg.sender;
   }
 
+  /// Returns this contracts counter.
   function getCounter() public returns (uint256) {
 	return _count;
   }
@@ -28,6 +33,28 @@ contract CallableCounter {
   function incrementPeerCounter() public {
 	_peer.incrementCounter();
 	_sender = msg.sender;
+  }
+
+  /// Recursively calls peer until the tail counter is incremented.
+  function incrementTailCounter() public {
+	if (_peer.isTail()) {
+	  _peer.incrementCounter();
+	  _sender = msg.sender;
+	} else {
+	  _peer.incrementTailCounter();
+	  _sender = msg.sender;
+	}
+  }
+
+  /// Returns the tail counter's count.
+  function getTailCounter() public returns (uint256) {
+	if (_peer.isTail()) {
+	  _sender = msg.sender;
+	  return _peer.getCounter();
+	} else {
+	  _sender = msg.sender;
+	  return _peer.getTailCounter();
+	}
   }
 
   function getPeerCounter() public returns (uint256) {
@@ -43,6 +70,24 @@ contract CallableCounter {
 	require(status);
   }
 
+    /// Recursively calls peer until the tail counter is incremented via delegatecall.
+  function incrementTailCounterDelegatecall() public {
+	  _sender = msg.sender;
+	  _peer.incrementPeerCounterDelegatecall();
+  }
+
+  function incrementPeerCounterDelegatecall() public {
+	if (_peer.isTail()) {
+	  _sender = msg.sender;
+	  bool status = address(_peer).delegatecall(bytes4(keccak256("receiveDelegatecallIncrementCounter()")));
+	  emit Status(status);
+	  require(status);
+	} else {
+	  _sender = msg.sender;
+	  incrementPeerCounterDelegatecall();
+	}
+  }
+
   /// Receives a message call through a delegatecall.
   function receiveDelegatecallIncrementCounter() public {
 	// This will not update my counter. It will update the counter
@@ -51,14 +96,7 @@ contract CallableCounter {
 	_sender = msg.sender;
   }
 
-  /// Makes a callcode to the peer, so that the peer invokes *my*
-  /// counter's storage. Does not preserve msg.sender.
-  function callcodeIncrementCounter() public {
-	// todo
-  }
-
-  /// Receives a message call through a callcode.
-  function receivecallcodeGetCounter() public {
-	// todo
+  function isTail() returns (bool) {
+	return _isTail;
   }
 }
