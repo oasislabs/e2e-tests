0a68640f4286426a188c79b2bf165f5e345e9462
armaniferrante <armaniferrante@gmail.com>
2019-03-12 20:07:11 +0000

    Tests passing

--- contracts/cross_contract/confidential/CallableCounter.sol
+++ contracts/cross_contract/confidential/CallableCounter.sol
@@ -11,92 +11,96 @@ contract CallableCounter {
   uint256 public _count;
   /// True iff this is the last counter in a linked ist.
   bool public _isTail;
+  /// The expected address of msg.sender when receiving a delegatecall.
+  address _expectedDelegatecallSender;
 
   event Status(bool status);
 
-  constructor(address peer, uint256 count, bool isTail) public {
-	_peer = CallableCounter(peer);
-	_count = count;
-	_isTail = isTail;
+  event Hi(address h);
+
+  constructor(address peer, uint256 count, bool isTail, address expectedDelegatecallSender) public {
+    _peer = CallableCounter(peer);
+    _count = count;
+    _isTail = isTail;
+    _expectedDelegatecallSender = expectedDelegatecallSender;
   }
 
   function incrementCounter() public {
-	_count = _count + 1;
-	_sender = msg.sender;
+    _count = _count + 1;
+    _sender = msg.sender;
   }
 
   /// Returns this contracts counter.
   function getCounter() public returns (uint256) {
-	return _count;
+    return _count;
   }
 
   function incrementPeerCounter() public {
-	_peer.incrementCounter();
-	_sender = msg.sender;
+    _peer.incrementCounter();
+    _sender = msg.sender;
   }
 
   /// Recursively calls peer until the tail counter is incremented.
   function incrementTailCounter() public {
-	if (_peer.isTail()) {
-	  _peer.incrementCounter();
-	  _sender = msg.sender;
-	} else {
-	  _peer.incrementTailCounter();
-	  _sender = msg.sender;
-	}
+    if (_peer.isTail()) {
+      _peer.incrementCounter();
+      _sender = msg.sender;
+    } else {
+      _peer.incrementTailCounter();
+      _sender = msg.sender;
+    }
   }
 
   /// Returns the tail counter's count.
   function getTailCounter() public returns (uint256) {
-	if (_peer.isTail()) {
-	  _sender = msg.sender;
-	  return _peer.getCounter();
-	} else {
-	  _sender = msg.sender;
-	  return _peer.getTailCounter();
-	}
+    if (_peer.isTail()) {
+      _sender = msg.sender;
+      return _peer.getCounter();
+    } else {
+      _sender = msg.sender;
+      return _peer.getTailCounter();
+    }
   }
 
   function getPeerCounter() public returns (uint256) {
-	return _peer.getCounter();
+    return _peer.getCounter();
   }
 
   /// Makes a delegatecall to the peer, so that the peer invokes *my*
   /// counter's storage. Preserves msg.sender.
   function delegatecallIncrementCounter() public {
     bool status = address(_peer).delegatecall(bytes4(keccak256("receiveDelegatecallIncrementCounter()")));
-	// WTF why is this needed (also need with ganache).
-	emit Status(status);
-	require(status);
+    // WTF why is this needed (also need with ganache).
+    emit Status(status);
+    require(status);
   }
 
-    /// Recursively calls peer until the tail counter is incremented via delegatecall.
   function incrementTailCounterDelegatecall() public {
-	  _sender = msg.sender;
-	  _peer.incrementPeerCounterDelegatecall();
+    require(msg.sender == _expectedDelegatecallSender);
+
+    _sender = msg.sender;
+
+    bool status = address(_peer).delegatecall(bytes4(keccak256("incrementDelegatecallProxy()")));
+    emit Status(status);
+    require(status);
   }
 
-  function incrementPeerCounterDelegatecall() public {
-	if (_peer.isTail()) {
-	  _sender = msg.sender;
-	  bool status = address(_peer).delegatecall(bytes4(keccak256("receiveDelegatecallIncrementCounter()")));
-	  emit Status(status);
-	  require(status);
-	} else {
-	  _sender = msg.sender;
-	  incrementPeerCounterDelegatecall();
-	}
+  function incrementDelegatecallProxy() public {
+    require(msg.sender == _expectedDelegatecallSender);
+
+    _sender = msg.sender;
+    _peer.receiveDelegatecallIncrementCounter();
   }
 
   /// Receives a message call through a delegatecall.
   function receiveDelegatecallIncrementCounter() public {
-	// This will not update my counter. It will update the counter
-	// of whoever delegatecalled into this method.
-	_count = _count + 1;
-	_sender = msg.sender;
+    // This will not update my counter. It will update the counter
+    // of whoever delegatecalled into this method.
+    _count = _count + 1;
+    _sender = msg.sender;
   }
 
-  function isTail() returns (bool) {
-	return _isTail;
+  function isTail() public returns (bool) {
+    return _isTail;
   }
 }
--- test/11_test_confidential_cross_contract.js
+++ test/11_test_confidential_cross_contract.js
@@ -10,266 +10,261 @@ if (truffleConfig.shouldRun(__filename)) {
   contract('Confidential Cross Contract Calls', function (accounts) {
     const options = { from: accounts[0] };
 
-    let callableContract = new web3c.oasis.Contract(CallableCounter.abi, undefined, options);
+    //let callableContract = new web3c.oasis.Contract(CallableCounter.abi, undefined, options);
+    let callableContract = new web3c.eth.Contract(CallableCounter.abi, undefined, options);
 
-	let bilateralTestCases = [
-	  {
-		a: {
-		  label: 'non-confidential',
-		  confidential: false
-		},
-		b: {
-		  label: 'non-confidential',
-		  confidential: false
-		},
-	  },
-	  {
-		a: {
-		  label: 'confidential',
-		  confidential: true
-		},
-		b: {
-		  label: 'confidential',
-		  confidential: true
-		},
-	  },
-	  {
-		a: {
-		  label: 'non-confidential',
-		  confidential: true
-		},
-		b: {
-		  label: 'confidential',
-		  confidential: true
-		},
-	  },
-	  {
-		a: {
-		  label: 'confidential',
-		  confidential: true
-		},
-		b: {
-		  label: 'non-confidential',
-		  confidential: true
-		},
-	  }
-	]
+    let bilateralTestCases = [
+      {
+        a: {
+          label: 'non-confidential',
+          confidential: false
+        },
+        b: {
+          label: 'non-confidential',
+          confidential: false
+        },
+      },
+      {
+        a: {
+          label: 'confidential',
+          confidential: true
+        },
+        b: {
+          label: 'confidential',
+          confidential: true
+        },
+      },
+      {
+        a: {
+          label: 'non-confidential',
+          confidential: true
+        },
+        b: {
+          label: 'confidential',
+          confidential: true
+        },
+      },
+      {
+        a: {
+          label: 'confidential',
+          confidential: true
+        },
+        b: {
+          label: 'non-confidential',
+          confidential: true
+        },
+      }
+    ];
 
     let a;
     let b;
 
-	bilateralTestCases.forEach((testCase) => {
-	  it(`bilateral: deploys a ${testCase.a.label} and a ${testCase.b.label} contract` , async () => {
-		b = await callableContract.deploy({
+    bilateralTestCases.forEach((testCase) => {
+      it(`bilateral: deploys a ${testCase.a.label} and a ${testCase.b.label} contract` , async () => {
+        b = await callableContract.deploy({
           data: CallableCounter.bytecode,
-          arguments: [accounts[0], 1, true],
+          arguments: [accounts[0], 1, true, accounts[0]],
           header: {
-			confidential: testCase.b_confidential
+            confidential: testCase.b_confidential
           }
-		}).send();
-		a = await callableContract.deploy({
+        }).send();
+        a = await callableContract.deploy({
           data: CallableCounter.bytecode,
-          arguments: [b.options.address, 0, false],
+          arguments: [b.options.address, 0, false, accounts[0]],
           header: {
-			confidential: testCase.a_confidential
+            confidential: testCase.a_confidential
           }
-		}).send();
+        }).send();
       });
 
       it(`bilateral: ${testCase.a.label} calls ${testCase.b.label}`, async () => {
-		// Given contracts A and B.
+        // Given contracts A and B.
 
-		// When.
-		let aCount = await a.methods.getCounter().call();
-		let aPeerCount = await a.methods.getPeerCounter().call();
+        // When.
+        let aCount = await a.methods.getCounter().call();
+        let aPeerCount = await a.methods.getPeerCounter().call();
+        let aSender = await a.methods._sender().call();
+        let bSender = await b.methods._sender().call();
 
-		// Then.
-		assert.equal(aCount, 0);
-		assert.equal(aPeerCount, 1);
+        // Then.
+        assert.equal(aCount, 0);
+        assert.equal(aPeerCount, 1);
+        // Not set, so defaults to 0.
+        assert.equal(aSender, 0);
+        assert.equal(bSender, 0);
       });
 
       it(`bilateral: ${testCase.a.label} sets ${testCase.b.label} storage via call`, async () => {
-		// Given, contracts A and B.
+        // Given, contracts A and B.
 
-		// When.
-		await a.methods.incrementPeerCounter().send();
+        // When.
+        await a.methods.incrementPeerCounter().send();
 
-		// Then.
-		let aCount = await a.methods.getCounter().call();
-		let bCount = await b.methods.getCounter().call();
+        // Then.
+        let aCount = await a.methods.getCounter().call();
+        let bCount = await b.methods.getCounter().call();
 
-		let aSender = await a.methods._sender().call();
-		let bSender = await b.methods._sender().call();
+        let aSender = await a.methods._sender().call();
+        let bSender = await b.methods._sender().call();
 
-		// Unchanged.
-		assert.equal(aCount, 0);
-		// Incremented.
-		assert.equal(bCount, 2);
-		// Sender is originator of transaction.
-		assert.equal(aSender, accounts[0]);
-		// Sender is contract A because it invoked a call.
-		assert.equal(bSender, a.options.address);
+        // Unchanged.
+        assert.equal(aCount, 0);
+        // Incremented.
+        assert.equal(bCount, 2);
+        // Sender is originator of transaction.
+        assert.equal(aSender, accounts[0]);
+        // Sender is contract A because it invoked a call.
+        assert.equal(bSender, a.options.address);
       });
 
       it(`bilateral: ${testCase.a.label} sets ${testCase.b.label} storage via delegatecall`, async () => {
-		// Given, contracts A and B.
+        // Given, contracts A and B.
 
-		// When.
-		await a.methods.delegatecallIncrementCounter().send();
+        // When.
+        await a.methods.delegatecallIncrementCounter().send();
 
-		// Then.
-		let aCount = await a.methods.getCounter().call();
-		let bCount = await b.methods.getCounter().call();
+        // Then.
+        let aCount = await a.methods.getCounter().call();
+        let bCount = await b.methods.getCounter().call();
 
-		let aSender = await a.methods._sender().call();
-		let bSender = await b.methods._sender().call();
-
-		// Incremented.
-		assert.equal(aCount, 1);
-		// Unchanged.
-		assert.equal(bCount, 2);
-		// Sender is originator of transaction.
-		assert.equal(aSender, accounts[0]);
-		// Sender is contract A because it invoked a call.
-		assert.equal(bSender, a.options.address);
+        // Incremented.
+        assert.equal(aCount, 1);
+        // Unchanged.
+        assert.equal(bCount, 2);
       });
-	});
+    });
 
-	let c = undefined;
+    let c = undefined;
 
-	let threePartyCases = [
-	  {
-		a: {
-		  label: 'non-confidential',
-		  confidential: false
-		},
-		b: {
-		  label: 'non-confidential',
-		  confidential: false
-		},
-		c: {
-		  label: 'non-confidential',
-		  confidential: false
-		},
-	  },
-	  {
-		a: {
-		  label: 'confidential',
-		  confidential: true
-		},
-		b: {
-		  label: 'confidential',
-		  confidential: true
-		},
-		c: {
-		  label: 'confidential',
-		  confidential: true
-		},
-	  },
-	  {
-		a: {
-		  label: 'confidential',
-		  confidential: true
-		},
-		b: {
-		  label: 'non-confidential',
-		  confidential: true
-		},
-		c: {
-		  label: 'confidential',
-		  confidential: true
-		},
-	  }
+    let threePartyCases = [
+      {
+        a: {
+          label: 'non-confidential',
+          confidential: false
+        },
+        b: {
+          label: 'non-confidential',
+          confidential: false
+        },
+        c: {
+          label: 'non-confidential',
+          confidential: false
+        },
+      },
+      {
+        a: {
+          label: 'confidential',
+          confidential: true
+        },
+        b: {
+          label: 'confidential',
+          confidential: true
+        },
+        c: {
+          label: 'confidential',
+          confidential: true
+        },
+      },
+      {
+        a: {
+          label: 'confidential',
+          confidential: true
+        },
+        b: {
+          label: 'non-confidential',
+          confidential: true
+        },
+        c: {
+          label: 'confidential',
+          confidential: true
+        },
+      }
+    ];
 
-	];
 
+    threePartyCases.forEach((testCase) => {
 
-	threePartyCases.forEach((testCase) => {
-
-	  it(`3-party: deploys a ${testCase.a.label}, ${testCase.b.label} and a ${testCase.c.label} contract`, async () => {
-		c = await callableContract.deploy({
-		  data: CallableCounter.bytecode,
-          arguments: [accounts[0], 2, true],
-          header: {
-			confidential: testCase.b_confidential
-          }
-		}).send();
-		b = await callableContract.deploy({
+      it(`3-party: deploys a ${testCase.a.label}, ${testCase.b.label} and a ${testCase.c.label} contract`, async () => {
+        c = await callableContract.deploy({
           data: CallableCounter.bytecode,
-          arguments: [c.options.address, 1, false],
+          arguments: [accounts[0], 2, true, accounts[0]],
           header: {
-			confidential: testCase.b_confidential
+            confidential: testCase.b_confidential
           }
-		}).send();
-		a = await callableContract.deploy({
+        }).send();
+        b = await callableContract.deploy({
           data: CallableCounter.bytecode,
-          arguments: [b.options.address, 0, false],
+          arguments: [c.options.address, 1, false, accounts[0]],
           header: {
-			confidential: testCase.a_confidential
+            confidential: testCase.b_confidential
           }
-		}).send();
-	  });
-
-	  it(`3-party: ${testCase.a.label} calls ${testCase.c.label} through ${testCase.b.label}`, async () => {
-		// Given contracts A, B, C.
-
-		// When.
-		let cCount = await a.methods.getTailCounter().call();
-
-		// Then.
-		assert.equal(cCount, 2);
-		let aSender = await a.methods._sender().call();
-		let bSender = await b.methods._sender().call();
-		let cSender = await c.methods._sender().call();
-		// Not set so just 0.
-		assert.equal(aSender, 0);
-		assert.equal(bSender, 0);
-		assert.equal(cSender, 0);
+        }).send();
+        a = await callableContract.deploy({
+          data: CallableCounter.bytecode,
+          arguments: [b.options.address, 0, false, accounts[0]],
+          header: {
+            confidential: testCase.a_confidential
+          }
+        }).send();
       });
 
-	  it(`3-party: ${testCase.a.label} sets storage of ${testCase.c.label} through ${testCase.b.label}`, async () => {
-		// Given contracts A, B, C.
+      it(`3-party: ${testCase.a.label} calls ${testCase.c.label} through ${testCase.b.label}`, async () => {
+        // Given contracts A, B, C.
 
-		// When.
-		await a.methods.incrementTailCounter().send();
-		let cCount = await c.methods.getCounter().call();
+        // When.
+        let cCount = await a.methods.getTailCounter().call();
 
-		// Then.
-		assert.equal(cCount, 3);
-		let aSender = await a.methods._sender().call();
-		let bSender = await b.methods._sender().call();
-		let cSender = await c.methods._sender().call();
-		// Set via call so should be the contract that called it.
-		assert.equal(aSender, accounts[0]);
-		assert.equal(bSender, a.options.address);
-		assert.equal(cSender, b.options.address);
+        // Then.
+        assert.equal(cCount, 2);
+        let aSender = await a.methods._sender().call();
+        let bSender = await b.methods._sender().call();
+        let cSender = await c.methods._sender().call();
+        // Not set so just 0.
+        assert.equal(aSender, 0);
+        assert.equal(bSender, 0);
+        assert.equal(cSender, 0);
       });
 
-	  it(`3-party: ${testCase.a.label} sets storage of ${testCase.c.label} through ${testCase.b.label} via delegatecall`, async () => {
-		// Given contracts A, B, C.
+      it(`3-party: ${testCase.a.label} sets storage of ${testCase.c.label} through ${testCase.b.label}`, async () => {
+        // Given contracts A, B, C.
 
-		// When.
-		await a.methods.incrementTailCounterDelegatecall().send({
-		  gas: '0xf42400'
-		});
-		let aCount = await a.methods.getCounter().call();
-		let bCount = await b.methods.getCounter().call();
-		let cCount = await c.methods.getCounter().call();
+        // When.
+        await a.methods.incrementTailCounter().send();
+        let cCount = await c.methods.getCounter().call();
 
-		// Then.
-		assert.equal(aCount, 0);
-		assert.equal(bCount, 2);
-		assert.equal(cCount, 3);
-		let aSender = await a.methods._sender().call();
-		let bSender = await b.methods._sender().call();
-		let cSender = await c.methods._sender().call();
-		// Set via delegatecall so should be the originator of the transaction (delegatecall
-		// preserves msg.sender).
-		assert.equal(aSender, accounts[0]);
-		assert.equal(bSender, a.options.address);
-		assert.equal(cSender, b.options.address);
+        // Then.
+        assert.equal(cCount, 3);
+        let aSender = await a.methods._sender().call();
+        let bSender = await b.methods._sender().call();
+        let cSender = await c.methods._sender().call();
+        // Set via call so should be the contract that called it.
+        assert.equal(aSender, accounts[0]);
+        assert.equal(bSender, a.options.address);
+        assert.equal(cSender, b.options.address);
       });
-	});
+
+      it(`3-party: ${testCase.a.label} sets storage of ${testCase.c.label} through ${testCase.b.label} via delegatecall`, async () => {
+        // Given contracts A, B, C.
+
+        // When.
+        await a.methods.incrementTailCounterDelegatecall().send({
+          gas: '0xf42400'
+        });
+
+        let aCount = await a.methods.getCounter().call();
+        let bCount = await b.methods.getCounter().call();
+        let cCount = await c.methods.getCounter().call();
+
+        // Then.
+        assert.equal(aCount, 0);
+        assert.equal(bCount, 2);
+        assert.equal(cCount, 3);
+
+        // Don't check _sender, since the storage will only affect a's storage
+        // due to the delegate call. To compensate, we assert
+        // msg.sender == _expectedDelegatecallSender inside the contract itself;
+      });
+    });
 
   });
 }
