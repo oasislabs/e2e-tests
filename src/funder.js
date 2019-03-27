0fca2bdd1b4d0a8afb6cb66abb4056df8fba1475
Peter Gilbert <pgilbert@oasislabs.com>
2019-03-27 15:44:21 -0700

    lint

--- src/funder.js
+++ src/funder.js
@@ -1,4 +1,4 @@
-const bip39 = require('bip39')
+const bip39 = require('bip39');
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const parseArgs = require('minimist');
 const request = require('request-promise');
@@ -8,15 +8,15 @@ const request = require('request-promise');
  * Will only work in cluster (staging or prod).
  */
 async function requestFunds (address, faucet) {
-    // Request 1 DEV.
-    let url = faucet + '?to=' + address + '&amnt=0xde0b6b3a7640000';
-    await request(url);
+  // Request 1 DEV.
+  let url = faucet + '?to=' + address + '&amnt=0xde0b6b3a7640000';
+  await request(url);
 }
 
 // Parse command-line arguments.
 const argv = parseArgs(process.argv.slice(2));
 if (argv._.length !== 2) {
-  console.error("Usage: funder <gateway_https_endpoint> <private_faucet_endpoint>");
+  console.error('Usage: funder <gateway_https_endpoint> <private_faucet_endpoint>');
   process.exit(1);
 }
 let gateway = argv._[0];
@@ -29,7 +29,7 @@ let address = wallet.addresses[0];
 
 // Request funds from the faucet.
 requestFunds(address, faucet).catch(error => {
-  console.error("Could not get funds from API faucet.");
+  console.error('Could not get funds from API faucet: ' + error);
   process.exit(1);
 }).then(() => {
   // Print the generated mnemonic.
