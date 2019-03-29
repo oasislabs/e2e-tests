45b1e4723e17e934462f898a98a949ec791c19f6
Peter Gilbert <pgilbert@oasislabs.com>
2019-03-28 17:08:58 -0700

    Update runner script

--- scripts/runner.sh
+++ scripts/runner.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-################################################################################
+####################################################################################
 # Get funds from API faucet and run e2e tests against a specified gateway.
 # Only works in-cluster, as the API faucet is not exposed externally.
 #
@@ -9,11 +9,11 @@
 # Required Args:
 #
 # - HTTPS_PROVIDER_URL: The gateway http endpoint (e.g., https://web3.oasiscloud.io).
-# - WS_PROVIDER_URL:    Gateway's websocket endpoint (e.g., wss://web.oasiscloud.io/ws).
-# - FAUCET_URL:         Faucet API endpoint.
-# - Optional extra args are passed to truffle test.
+# - WS_PROVIDER_URL:    Gateway websocket endpoint (e.g., wss://web.oasiscloud.io/ws).
+# - FAUCET_URL:         API faucet endpoint.
+# - Optional extra args are passed to 'truffle test'.
 #
-################################################################################
+####################################################################################
 
 # Helpful tips on writing build scripts:
 # https://buildkite.com/docs/pipelines/writing-build-scripts
@@ -22,12 +22,14 @@ set -euxo pipefail
 # Skip compilation step.
 export SKIP_OASIS_COMPILE=true
 
-# Gateway endpoints.
+# Export gateway endpoints for truffle.
 export HTTPS_PROVIDER_URL=$1
 export WS_PROVIDER_URL=$2
 
+FAUCET_URL=$3
+
 # Generate a mnemonic and request funds from the API faucet.
-mnemonic=`node src/funder.js $HTTPS_PROVIDER_URL $3`
+mnemonic=`node src/funder.js $HTTPS_PROVIDER_URL $FAUCET_URL`
 export MNEMONIC=$mnemonic
 
 # Run truffle tests.
