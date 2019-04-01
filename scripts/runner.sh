dea3c56c0078433948cfcb5c101c37e8b6755910
Peter Gilbert <petergilbert@gmail.com>
2019-04-01 11:33:29 -0700

    Add docker image for running tests against staging in cluster (#57)

---
+++ scripts/runner.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+####################################################################################
+# Get funds from API faucet and run e2e tests against a specified gateway.
+# Only works in-cluster, as the API faucet is not exposed externally.
+#
+# ./runner.sh <HTTPS_PROVIDER_URL> <WS_PROVIDER_URL> <FAUCET_URL> [...]
+#
+# Required Args:
+#
+# - HTTPS_PROVIDER_URL: The gateway http endpoint (e.g., https://web3.oasiscloud.io).
+# - WS_PROVIDER_URL:    Gateway websocket endpoint (e.g., wss://web.oasiscloud.io/ws).
+# - FAUCET_URL:         API faucet endpoint.
+# - Optional extra args are passed to 'truffle test'.
+#
+####################################################################################
+
+# Helpful tips on writing build scripts:
+# https://buildkite.com/docs/pipelines/writing-build-scripts
+set -euxo pipefail
+
+# Skip contract compilation step (contracts are prebuilt).
+export SKIP_OASIS_COMPILE=true
+
+# Export gateway endpoints for truffle.
+export HTTPS_PROVIDER_URL=$1
+export WS_PROVIDER_URL=$2
+
+FAUCET_URL=$3
+
+# Generate a mnemonic and request funds from the API faucet.
+mnemonic=`node src/funder.js $HTTPS_PROVIDER_URL $FAUCET_URL`
+export MNEMONIC=$mnemonic
+
+# Run truffle tests.
+shift 3
+npm run test:development $*
