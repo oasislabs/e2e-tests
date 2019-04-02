691d42d7ac6b6d1adfa0508c6ed5aacebef6a448
Peter Gilbert <pgilbert@oasislabs.com>
2019-04-02 16:54:29 -0700

    Add error counter prometheus metric

--- src/pusher.js
+++ src/pusher.js
@@ -12,4 +12,14 @@ let name = argv._[1];
 let result = argv._[2];
 
 let pushGateway = new promClient.Pushgateway(gateway, { timeout: 5000 });
+
+// Initialize error counter to 0.
+const err = new promClient.Gauge({ name: 'e2e_tests_err', help: 'Error in e2e tests.' });
+err.reset();
+
+// If exit code was not 0, report an error.
+if (result !== 0) {
+  err.inc();
+}
+
 pushGateway.pushAdd({ jobName: `${name}`, groupings: 'ops-production' }, () => { process.exit(result); });
