fc0e9e19af7d57c66b6936e1e3cb16172707674f
Armani Ferrante <armaniferrante@gmail.com>
2019-04-10 22:18:21 -0700

    Log

--- test/11_test_failures.js
+++ test/11_test_failures.js
@@ -124,6 +124,7 @@ if (truffleConfig.shouldRun(__filename)) {
         });
         assert.fail(new Error('error should have been thrown'));
       } catch (e) {
+        console.log(e);
         assert.equal(e.message.includes('Unable to decrypt transaction data: invalid nonce or public key'), true);
       }
     });
