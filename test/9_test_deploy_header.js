e58a0877ee3668db23aef6b7e98354e150463b05
Peter Gilbert <pgilbert@oasislabs.com>
2019-05-15 16:57:19 -0700

    Remove uses of confidential call and estimateGas (#81)

--- test/9_test_deploy_header.js
+++ test/9_test_deploy_header.js
@@ -1,23 +1,21 @@
 const Counter = artifacts.require('Counter');
 const truffleConfig = require('../truffle-config');
-const Web3c = require('web3c');
-const web3c = new Web3c(Counter.web3.currentProvider, undefined, {
-  keyManagerPublicKey: truffleConfig.KEY_MANAGER_PUBLIC_KEY
-});
 const _assert = require('assert');
 const utils = require('../src/utils');
+const web3c = utils.setupWebsocketProvider(Counter.web3.currentProvider);
 
 if (truffleConfig.shouldRun(__filename)) {
   contract('Deploy Header', async (accounts) => {
     let contract = new web3c.oasis.Contract(Counter.abi, undefined, {
-      from: accounts[0]
+      from: accounts[0],
+      gas: '0x100000'
     });
     let labels = ['confidential', 'eth'];
     labels.forEach((label) => {
       let instance;
       let confidential = label === 'confidential';
 
-      it(`${label}: creates a confidential contract that expires tomorrow with success`, async () => {
+      it(`${label}: creates a contract that expires tomorrow with success`, async () => {
         // Given.
         let counterContract = contract;
 
@@ -29,7 +27,11 @@ if (truffleConfig.shouldRun(__filename)) {
             expiry: expectedExpiry,
             confidential
           }
-        }).send();
+        }).send({
+          // Hardcode gas if confidential.
+          // TODO: tighter bound for gasLimit
+          ...(confidential && { gasLimit: 1000000 })
+        });
 
         // Then.
         let resultantExpiry = await instance.expiry();
@@ -41,15 +43,19 @@ if (truffleConfig.shouldRun(__filename)) {
       });
 
       it(`${label}: can execute transactions and calls on a contract with expiry`, async () => {
-        let count = await instance.methods.getCounter().call();
+        let count = await instance.methods.getCounter().invoke();
         assert.equal(count, 0);
-        await instance.methods.incrementCounter().send();
-        count = await instance.methods.getCounter().call();
+        await instance.methods.incrementCounter().send({
+          // Hardcode gas if confidential.
+          // TODO: tighter bound for gasLimit
+          ...(confidential && { gasLimit: 1000000 })
+        });
+        count = await instance.methods.getCounter().invoke();
         assert.equal(count, 1);
       });
 
       it(`${label}: creates a contract that expires yesterday with failure`, async () => {
-        _assert.rejects(
+        await _assert.rejects(
           async function () {
             // Given.
             let counterContract = contract;
@@ -62,7 +68,11 @@ if (truffleConfig.shouldRun(__filename)) {
                 expiry: expectedExpiry,
                 confidential
               }
-            }).send();
+            }).send({
+              // Hardcode gas if confidential.
+              // TODO: tighter bound for gasLimit
+              ...(confidential && { gasLimit: 1000000 })
+            });
 
             // Then reject.
           }
@@ -77,18 +87,27 @@ if (truffleConfig.shouldRun(__filename)) {
             expiry: Math.floor(Date.now() / 1000 + 20),
             confidential
           }
-        }).send();
+        }).send({
+          // Hardcode gas if confidential.
+          // TODO: tighter bound for gasLimit
+          ...(confidential && { gasLimit: 1000000 })
+        });
         // When.
         await utils.sleep(60 * 1000);
         // Send dummy transaction to make the tendermint clock tick.
         await web3c.eth.sendTransaction({
           from: accounts[0],
           to: web3c.eth.accounts.create().address,
-          value: 100
+          value: 100,
+          gas: 2100
         });
         // Then.
         _assert.rejects(async function () {
-          await instance.methods.incrementCounter().send();
+          await instance.methods.incrementCounter().send({
+            // Hardcode gas if confidential.
+            // TODO: tighter bound for gasLimit
+            ...(confidential && { gasLimit: 1000000 })
+          });
         });
       });
     });
