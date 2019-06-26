b6223fd4207684d547e1835591261c3575d22a65
Armani Ferrante <armaniferrante@gmail.com>
2019-06-26 12:41:18 -0700

    Use client branch

--- scripts/oasis-client.sh
+++ scripts/oasis-client.sh
@@ -17,6 +17,7 @@ mkdir tmp
 pushd tmp
     git clone https://github.com/oasislabs/oasis-client.git
     pushd oasis-client
+      git checkout armani/test
       yarn
       yarn build
       ln -s $(pwd)/node_modules/client $(pwd)/../../node_modules/client
