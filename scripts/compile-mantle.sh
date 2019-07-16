983f96e7b4084347814f6249272329ba9ea5e993
Armani Ferrante <armaniferrante@gmail.com>
2019-07-15 21:46:34 -0700

    Compile crates in background

--- scripts/compile-mantle.sh
+++ scripts/compile-mantle.sh
@@ -1,5 +1,17 @@
 #! /bin/bash
 
-pushd mantle/mantle-counter
-    oasis build --release
+echo "Buliding mantle services"
+pushd mantle
+    for d in */ ; do
+        pushd $d
+            echo "Buliding $d..."
+            oasis build --release &
+        popd
+    done
+
+    for job in `jobs -p`
+    do
+        wait $job
+    done
 popd
+echo "Done buliding mantle services"
