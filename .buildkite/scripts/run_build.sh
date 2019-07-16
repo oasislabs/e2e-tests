d6a61e3da2c259bd42523b4fd53394944694b1cb
Armani Ferrante <armaniferrante@gmail.com>
2019-07-15 20:02:29 -0700

    Fiixes

--- .buildkite/scripts/run_build.sh
+++ .buildkite/scripts/run_build.sh
@@ -42,19 +42,23 @@ rustup default nightly
 cargo install --git https://github.com/oasislabs/oasis-cli.git oasis-cli
 cargo install --git https://github.com/oasislabs/mantle.git mantle-build
 # So we ignore oasis-cli prompts.
+mkdir -p ~/.config/oasis/
 touch ~/.config/oasis/config.toml
 
 # Install e2e-test dependencies.
-npm install
+#npm install
 
 # Lint Check.
-npm run lint
+#npm run lint
 
 # Compile the contracts.
-npm run compile:truffle
+#npm run compile:truffle
 npm run compile:mantle
 
 # Finally move the build to the out directory.
 zip -r build.zip build
 mkdir -p $out_dir
 mv build.zip $out_dir
+
+zip -r mantle.zip mantle
+mv mantle.zip $out_dir
