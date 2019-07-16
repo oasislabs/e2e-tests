595f76c2e63aa5e2e09811daed9244750d55fc88
Armani Ferrante <armaniferrante@gmail.com>
2019-07-16 11:25:08 -0700

    Remove continue

--- test/13_test_oasis_client.js
+++ test/13_test_oasis_client.js
@@ -59,75 +59,73 @@ if (truffleConfig.shouldRun(__filename)) {
     services.forEach(serviceConfig => {
       gateways.forEach(gatewayConfig => {
         headers.forEach(headerConfig => {
-
           // We don't support confidential solidity.
-          if (headerConfig.header.confidential && serviceConfig.label === 'solidity') {
-            continue;
-          }
+          if (!headerConfig.header.confidential || serviceConfig.label !== 'solidity') {
 
-          let prefix = `${serviceConfig.label}/${gatewayConfig.label}/${headerConfig.label}`;
-          let service;
+            let prefix = `${serviceConfig.label}/${gatewayConfig.label}/${headerConfig.label}`;
+            let service;
 
-          it(`${prefix}: sets the default gateway`, () => {
-            oasis.setGateway(gatewayConfig.gateway);
-          });
-
-          it(`${prefix}: deploys a service`, async () => {
-            service = await oasis.deploy({
-              idl: serviceConfig.idl,
-              bytecode: serviceConfig.bytecode,
-              arguments: [],
-              header: headerConfig.header,
-              coder: serviceConfig.coder
+            it(`${prefix}: sets the default gateway`, () => {
+              oasis.setGateway(gatewayConfig.gateway);
             });
-          });
 
-          it(`${prefix}: executes an rpc`, async () => {
-            let beforeCount = await service.getCounter(gatewayConfig.options);
-            let result = await service.incrementCounter(gatewayConfig.options);
-            let afterCount = await service.getCounter(gatewayConfig.options);
-            let setResult = await service.setCounter(6, gatewayConfig.options);
-            let afterSetCount = await service.getCounter(gatewayConfig.options);
-            let setResult2 = await service.setCounter2(10, 9, gatewayConfig.options);
-            let afterSetCount2 = await service.getCounter(gatewayConfig.options);
+            it(`${prefix}: deploys a service`, async () => {
+              service = await oasis.deploy({
+                idl: serviceConfig.idl,
+                bytecode: serviceConfig.bytecode,
+                arguments: [],
+                header: headerConfig.header,
+                coder: serviceConfig.coder
+              });
+            });
 
-            assert.equal(beforeCount, 0);
-            assert.equal(result, null);
-            assert.equal(afterCount, 1);
-            assert.equal(afterSetCount, 6);
-            assert.equal(setResult, null);
-            assert.equal(afterSetCount2, 9);
-            assert.equal(setResult2, null);
-          });
+            it(`${prefix}: executes an rpc`, async () => {
+              let beforeCount = await service.getCounter(gatewayConfig.options);
+              let result = await service.incrementCounter(gatewayConfig.options);
+              let afterCount = await service.getCounter(gatewayConfig.options);
+              let setResult = await service.setCounter(6, gatewayConfig.options);
+              let afterSetCount = await service.getCounter(gatewayConfig.options);
+              let setResult2 = await service.setCounter2(10, 9, gatewayConfig.options);
+              let afterSetCount2 = await service.getCounter(gatewayConfig.options);
 
-          it(`${prefix}: listens for three service events`, async () => {
-            let logs = await new Promise(async resolve => {
-              let logs = [];
-              service.addEventListener('Incremented', function listener (event) {
-                logs.push(event);
-                if (logs.length === 3) {
-                  service.removeEventListener('Incremented', listener);
-                  resolve(logs);
+              assert.equal(beforeCount, 0);
+              assert.equal(result, null);
+              assert.equal(afterCount, 1);
+              assert.equal(afterSetCount, 6);
+              assert.equal(setResult, null);
+              assert.equal(afterSetCount2, 9);
+              assert.equal(setResult2, null);
+            });
+
+            it(`${prefix}: listens for three service events`, async () => {
+              let logs = await new Promise(async resolve => {
+                let logs = [];
+                service.addEventListener('Incremented', function listener (event) {
+                  logs.push(event);
+                  if (logs.length === 3) {
+                    service.removeEventListener('Incremented', listener);
+                    resolve(logs);
+                  }
+                });
+                for (let k = 0; k < 3; k += 1) {
+                  await service.incrementCounter();
                 }
               });
-              for (let k = 0; k < 3; k += 1) {
-                await service.incrementCounter();
+
+              for (let k = 1; k < logs.length; k += 1) {
+                // Depending upon the gateway's view, we might get the log for the previous test,
+                // so just ensure the logs received are monotonically increasing.
+                let currentCounter = logs[k].newCounter;
+                let lastCounter = logs[k-1].newCounter;
+                // Solidity coder uses big numbers.
+                if (serviceConfig.label === 'solidity') {
+                  currentCounter = currentCounter.toNumber();
+                  oldCounter = oldCounter.toNumber();
+                }
+                assert.equal(currentCounter - lastCounter, 1);
               }
             });
-
-            for (let k = 1; k < logs.length; k += 1) {
-              // Depending upon the gateway's view, we might get the log for the previous test,
-              // so just ensure the logs received are monotonically increasing.
-              let currentCounter = logs[k].newCounter;
-              let lastCounter = logs[k-1].newCounter;
-              // Solidity coder uses big numbers.
-              if (serviceConfig.label === 'solidity') {
-                currentCounter = currentCounter.toNumber();
-                oldCounter = oldCounter.toNumber();
-              }
-              assert.equal(currentCounter - lastCounter, 1);
-            }
-          });
+          }
         });
       });
     });
