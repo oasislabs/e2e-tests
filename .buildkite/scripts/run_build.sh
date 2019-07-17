d5cd92301f6a866445851359769506e081459680
Armani Ferrante <armaniferrante@gmail.com>
2019-07-17 10:49:35 -0700

    apt-get install

--- .buildkite/scripts/run_build.sh
+++ .buildkite/scripts/run_build.sh
@@ -37,11 +37,14 @@ ssh-add || true
 ################################################################################
 
 # Install the latest oasis-bulid deps.
-cargo install --git https://github.com/oasislabs/oasis-cli.git oasis-cli
-cargo install --git https://github.com/oasislabs/oasis-rs.git oasis-build
+#cargo install --git https://github.com/oasislabs/oasis-cli.git oasis-cli
+#cargo install --git https://github.com/oasislabs/oasis-rs.git oasis-build
 # So we can skip the oasis-cli startup prompts.
 mkdir -p ~/.config/oasis/
 touch ~/.config/oasis/config.toml
+
+apt-get update
+apt-get install nodejs
 
 # Install e2e-test dependencies.
 npm install
