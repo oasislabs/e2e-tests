4adbef048ac4099d0e6cac34ed3ba6566fcb57d1
Armani Ferrante <armaniferrante@gmail.com>
2019-07-24 14:55:14 -0700

    Disable dev-gateway again

--- test/8_test_oasis_client.js
+++ test/8_test_oasis_client.js
@@ -30,7 +30,7 @@ if (truffleConfig.shouldRun(__filename)) {
         completion: test => test.gateway.disconnect(),
         options: { gasLimit: '0xe79732' },
         label: 'web3-gw'
-      },
+      }/*,
       {
         gateway: new oasis.gateways.Gateway(truffleConfig.DEVELOPER_GATEWAY_URL, {
           headers: new Map([['X-OASIS-INSECURE-AUTH', 'VALUE']])
@@ -38,7 +38,7 @@ if (truffleConfig.shouldRun(__filename)) {
         completion: _test => {},
         options: undefined,
         label: 'dev-gw'
-      }
+      }*/
     ];
     const headers = [
       {
