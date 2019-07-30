9b3353aaa0a68fa418a394eaaeeabcba2bee1288
eauge <estanislauap@oasislabs.com>
2019-07-30 10:26:31 -0700

    enable developer-gateway in e2e tests (#107)
    * enable developer-gateway in e2e tests
    
    * Update 8_test_oasis_client.js
    
    * Update 8_test_oasis_client.js

--- test/8_test_oasis_client.js
+++ test/8_test_oasis_client.js
@@ -30,15 +30,14 @@ if (truffleConfig.shouldRun(__filename)) {
         completion: test => test.gateway.disconnect(),
         options: { gasLimit: '0xe79732' },
         label: 'web3-gw'
-      }/*,
-         {
-         gateway: new oasis.gateways.Gateway(truffleConfig.DEVELOPER_GATEWAY_URL, {
-         headers: new Map([['X-OASIS-INSECURE-AUTH', 'VALUE']])
-         }),
-         completion: _test => {},
-         options: undefined,
-         label: 'dev-gw'
-         } */
+      }, {
+        gateway: new oasis.gateways.Gateway(truffleConfig.DEVELOPER_GATEWAY_URL, {
+          headers: new Map([['X-OASIS-INSECURE-AUTH', 'VALUE']])
+        }),
+        completion: _test => {},
+        options: undefined,
+        label: 'dev-gw'
+      }
     ];
     const headers = [
       {
